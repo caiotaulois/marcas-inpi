cdebaf63b5e1bce3d4852680ebfa8aa51e7d3c63
caio taulois <caiotaulois@gmail.com>
2019-09-29 18:02:38 -0300

    aprovados por classe, registros por pais estrangeiro, procuradores e estados sem procurador

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "marcas-inpi"
+
+version := "0.1"
+
+scalaVersion := "2.11.12"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
+libraryDependencies += "com.databricks" %% "spark-xml" % "0.5.0"
+libraryDependencies += "org.elasticsearch" % "elasticsearch-hadoop" % "7.3.2"
---
+++ src/main/scala/com.bigdata.spark/Marcas.scala
@@ -0,0 +1,87 @@
+package com.bigdata.spark
+
+import com.databricks.spark.xml._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.elasticsearch.spark.sql._
+
+object Marcas {
+
+  def main(args: Array[String]): Unit = {
+
+    if (args.length < 3) {
+      println("Por favor, inserir IP elasticsearch, porta elasticsearch e caminho para XMLs.")
+      return
+    }
+    val node = args(0)
+    val port = args(1)
+    val path = args(2)
+
+    val spark = iniciarSparkSession(node, port)
+    val marcas = spark.read.option("rowTag", "processo").xml(path + "\\*.xml")
+    val classes = spark.read.option("header", "true").csv(path + "\\classe-nice.csv").as("classes")
+    aprovadosPorClasse(marcas, classes)
+    registrosPorPaisEstrangeiro(marcas)
+    procuradores(marcas)
+    estadosSemProcurador(marcas)
+  }
+
+  def iniciarSparkSession(node: String, port: String): SparkSession = {
+    SparkSession
+      .builder()
+      .master("local")
+      .appName("Marcas INPI")
+      .config("spark.es.nodes", node)
+      .config("spark.es.port", port)
+      .config("es.nodes.wan.only", "true")
+      .getOrCreate()
+  }
+
+  def aprovadosPorClasse(marcas: DataFrame, classesNice: DataFrame): Unit = {
+    marcas
+      .withColumn("despacho", explode(col("despachos.despacho")))
+      .filter(col("despacho._codigo") === "IPAS158")
+      .groupBy("classe-nice._codigo")
+      .count()
+      .withColumnRenamed("_codigo", "codigoClasse")
+      .withColumnRenamed("count", "registros")
+      .as("registrosPorClasse")
+      .join(classesNice, col("registrosPorClasse.codigoClasse") === col("classes.codigo"))
+      .select("registrosPorClasse.codigoClasse", "classe", "registros")
+      .saveToEs("classes/classe", Map("es.mapping.id" -> "codigoClasse"))
+  }
+
+  def registrosPorPaisEstrangeiro(marcas: DataFrame): Unit = {
+    marcas
+      .filter(col("titulares.titular._pais").isNotNull)
+      .filter(col("titulares.titular._pais")=!="BR")
+      .groupBy("titulares.titular._pais")
+      .count()
+      .withColumnRenamed("_pais", "pais")
+      .withColumnRenamed("count", "registros")
+      .select("pais", "registros")
+      .saveToEs("paises/pais", Map("es.mapping.id" -> "pais"))
+  }
+
+  def procuradores(marcas: DataFrame): Unit = {
+    marcas
+      .groupBy("procurador")
+      .count()
+      .withColumnRenamed("_procurador", "procurador")
+      .withColumnRenamed("count", "registros")
+      .select("procurador", "registros")
+      .saveToEs("procuradores/procurador", Map("es.mapping.id" -> "procurador"))
+  }
+
+  def estadosSemProcurador(marcas: DataFrame): Unit = {
+    marcas
+      .filter(col("procurador").isNull)
+      .filter(col("titulares.titular._pais")==="BR")
+      .groupBy("titulares.titular._uf")
+      .count()
+      .withColumnRenamed("_uf", "uf")
+      .withColumnRenamed("count", "registros")
+      .select("uf", "registros")
+      .saveToEs("ufs/uf", Map("es.mapping.id" -> "uf"))
+  }
+}
